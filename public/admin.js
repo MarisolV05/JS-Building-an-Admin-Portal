10c18617bc245f2d5daab97a2cc8462ed0c41de6
marisol Valenzuela <marisolvalenzuela@MacBook-Air.local>
2023-04-29 07:58:42 -0700

    buiding admin portal

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,50 @@
 
 // Your Code Here
+
+// Your Code Here
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+         
+         
+    let books = await response.json()
+
+    books.forEach(renderBook)
+
+}
+
+let updatedBook = await response.json();
+
+
+function renderBook(book) {
+    let root = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+     fetch('http://localhost:3001/updatedBook',{
+        method: 'patch',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+     
+        body: JSON.stringify({
+            id: book.id,
+            quantity: quantityInput.value
+        })
+    })
+    })
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
+}
+
+main();
+
